07e9b9c6e1186d66a549c3ef5fd372ab72f74960
tribi331 <45086801+tribi331@users.noreply.github.com>
2019-03-18 16:39:06 +0700

    dsds

--- js/main.js
+++ js/main.js
@@ -56,4 +56,16 @@ $(document).ready(function () {
             }
         }
     })
+    jQuery(document).ready(function ($) {
+        $("#mobile-menu").mobileMenu({
+            MenuWidth: 250,
+            SlideSpeed: 250,
+            WindowsMaxWidth: 767,
+            PagePush: true,
+            FromLeft: true,
+            Overlay: true,
+            CollapseMenu: true,
+            ClassName: "mobile-menu"
+        });
+    });
 });

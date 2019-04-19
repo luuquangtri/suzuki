d2f6c18b48c35e02f1f60f01b5b81f97d436f0cc
luuquangtri <48348502+luuquangtri@users.noreply.github.com>
2019-04-19 23:44:04 +0700

    dsds

--- themes/pluggin/main.js
+++ themes/pluggin/main.js
@@ -82,6 +82,31 @@ $(document).ready(function () {
             $('.navi').removeClass('fixed-2');
         }
     })
+    
+    $(".toggle-next ").click(function () {
+        event.preventDefault();
+        $(this).next().slideToggle();
+        $(this).toggleClass('active');
+    });
+    $(document).ready(function() {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+                }
+            }
+        });
+    });
     // $(".poi").on('mouseenter mouseleave', function () {
     //     $(this).find('.gray-overlay').fadeToggle(500);
     // });
